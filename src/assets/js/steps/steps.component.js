c02fd4b5379e8d9bb8477d74a2c712c14107e635
chrfreitas <chrfreitas@gmail.com>
2017-06-17 11:37:18 -0300

    Added .jshintrc and started step by step

--- src/assets/js/steps/steps.component.js
+++ src/assets/js/steps/steps.component.js
@@ -1,13 +1,42 @@
 (function(Vue){
 
+    'use strict';
+
+    const STATUS = {
+        WAITING: 0,
+        ACTIVE: 1,
+        COMPLETED: 2
+    };
+
     new Vue({
-        el: '.main',
+        el: '.main'
+    });
+
+    new Vue({
+        el: '.step',
+        data: {
+            status: {},
+            cssStatus: {
+                'step--done': STATUS.COMPLETED === this.data.status,
+                'step--active': STATUS.ACTIVE === this.data.status
+            }
+        },
+        computed: function(){
+            this.$data.status = STATUS.WAITING;
+        },
         methods: {
             next: function() {
-                console.log(1);
+                this.$data.status = STATUS.COMPLETED;
+
+                this.$emit('next');
+            },
+            previus: function() {
+                this.$data.status = STATUS.WAITING;
+
+                this.$emit('previus');
             }
         }
-
     });
+
 
 })(Vue);
